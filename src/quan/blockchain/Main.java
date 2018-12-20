5fdb5f7d0beeb64bd3ce91890983692486c09544
unknown <vozermuonnam1@gmail.com>
2018-12-20 22:09:07 +0700

    remove comments

--- src/quan/blockchain/Main.java
+++ src/quan/blockchain/Main.java
@@ -59,58 +59,5 @@ public class Main {
 					System.out.println("walletB: " + wallet.getBalance());
 					System.out.println();
 		}
-	}
-		/*
-		if(!checkWalletName) {
-			System.out.println("Do you want to create new wallet: ");
-			String string  = scanner.nextLine();
-			if(string.equals("y")) {
-				BlockChain.createNewWalet(walletName);
-				Wallet wallet = BlockChain.getWallet(walletName);
-				WalletUtils.commandWallet(wallet);
-			}
-		} else  {
-			Wallet wallet = BlockChain.getWallet(walletName);
-			WalletUtils.commandWallet(wallet);
-		}*/
-		
-		
-
-		/*System.out.println("\nWalletA is Attempting to send funds (40) to WalletB...");
-		block1.addTransaction(walletA.sendFund(walletB.publicKey, 40f));
-		addBlock(block1);
-		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
-		System.out.println("WalletB's balance is: " + walletB.getBalance());
-		
-		Block block2 = new Block(block1.hash);
-		System.out.println("\nWalletA Attempting to send more funds (1000) than it has...");
-		block2.addTransaction(walletA.sendFund(walletB.publicKey, 1000f));
-		addBlock(block2);
-		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
-		System.out.println("WalletB's balance is: " + walletB.getBalance());*/
-		
-		/*Block block2 = new Block("Block 3", block1.getHash());
-		System.out.println("WalletB sent 5 coin to WalletA: ");
-		block2.addTransaction(walletB.sendFund(walletA.publicKey, 0));
-		blockchain.blockchain.add(block2);*/
-		
-		/*String json = new GsonBuilder().setPrettyPrinting().create()
-				.toJson(blockchain.getBlockchainIntanse().get(blockchain.getBlockchainIntanse().size() - 1));
-		System.out.println("-------------");
-		System.out.println(json);*/
-
-		// create test transaction
-		/*Transaction transaction = new Transaction(wallet.publicKey, wallet2.publicKey, (float) 5.1, null);
-		transaction.generateSignature(wallet.privateKey);
-		System.out.println("check signature: " + transaction.checkSignature());*/
-
-		/*System.out.println("Start mining");
-		while (true) {
-			blockchain.miningBlock(5);
-			String json = new GsonBuilder().setPrettyPrinting().create()
-					.toJson(blockchain.getBlockchainIntanse().get(blockchain.getBlockchainIntanse().size() - 1));
-			System.out.println("-------------");
-			System.out.println(json);
-			System.out.println("Chain valid ?" + blockchain.isChainValid());
-		}*/
+	}			
 }
